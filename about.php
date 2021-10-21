a20a2447747e5897f5ac381fd8940877a8c49b94
ravenusmc <mcuddy77@gmail.com>
2021-10-21 10:00:37 -0400

    built out header section

--- about.php
+++ about.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <title name="description" content="The Lost American">The Lost American</title>
 	<!-- Link for Bulma -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 	<!-- links for Google Fonts -->
@@ -14,7 +6,6 @@
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
 	<!-- links for the web page -->
 	<link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
-</head>
-<body>
+	
 <?php include 'view/header.php'; ?>
 <?php include 'view/navbar.php'; ?>
---
+++ view/header.php
@@ -0,0 +1,9 @@
+<!-- Links for CSS -->
+<link rel="stylesheet" type="text/css" href="./assets/css/header.css">
+
+<!-- Header Section -->
+<section>
+	<h3 class='sub_title center font'>U.S. Marine. Intelligence Officer. Web Developer.</h3>
+	<h1 class='main_title center font'>The Lost American</h1>
+	<h3 class='sub_title center font'>Writer. Thinker. Developer. YouTuber.</h3>
+</section>
