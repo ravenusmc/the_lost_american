26557eb07e95291aa40b147e3657170fec241c91
ravenusmc <mcuddy77@gmail.com>
2021-10-21 08:43:26 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<!-- the head section -->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title name="description" content="The Lost American">The Lost American</title>
+  <!-- CSS Style Sheets -->
+  <link rel="stylesheet" type="text/css" href="../assets/css/generic.css">
+  <!-- links for the web page -->
+  <link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
+  <link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+</head>
+<body>
+<h1>Hello</h1>
