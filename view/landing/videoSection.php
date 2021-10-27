4c76e5405ff73faa4591a94c98566a3b97d7b94e
ravenusmc <mcuddy77@gmail.com>
2021-10-27 08:53:47 -0400

    worked on fixing minor design issues on navbar and video sections

--- view/landing/videoSection.php
+++ view/landing/videoSection.php
@@ -1,24 +1,7 @@
-<?php 
-	
-	//Pulling in the databases
-	// require('./model/database.php');
-
-	// global $db;
-	// $query = 'SELECT *
-	// 					FROM projects
-	// 					ORDER BY id ASC';
-	// $statement = $db->prepare($query);
-	// $statement->execute();
-	// $videos = $statement->fetchAll();
-	// $statement->closeCursor();
-
-	// print_r($videos);
-
-?>
 <!-- Links for CSS -->
 <link rel="stylesheet" type="text/css" href="./assets/css/videoSection.css">
 
-<section>
+<section id='video-section'>
 	<h1 class='center title font'>Video Area</h1>
 	<?php foreach ($projects as $project): ?>
 		<div class='video-div'>
--- view/navbar.php
+++ view/navbar.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" type="text/css" href="./assets/css/navbar.css">
 
 <!-- Navbar -->
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar is-dark" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="https://bulma.io">
     </a>
